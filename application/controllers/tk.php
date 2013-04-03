44de6def2b7929de55bf2fa286f943134b1b1961
Thusanthan Kannan <thusanthankannan@gmail.com>
2013-04-03 18:13:35 -0400

    made random changes

--- application/controllers/tk.php
+++ application/controllers/tk.php
@@ -6,12 +6,16 @@ class TK extends CI_Controller
 
 function toolegit()
 {
-	echo "Can't Touch This - na na na ";
+	echo "Type your name below";
 
 	$this->load->helper('form');
+
 	echo form_open();	
-	echo form_input('username', 'johndoe');
-	echo form_submit('mike', 'tk');
+	# Creating the imput box
+	echo form_input('username', '');
+	# Creating the button
+	echo form_submit('mike', 'Submit');
+
 	echo form_close();
 
 }
