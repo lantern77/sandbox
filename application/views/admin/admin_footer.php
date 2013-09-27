17392680001346f9c5e671096c4f7930fb55e163
root <root@genesis.genesis>
2013-09-27 04:24:01 -0400

    Added in Laravel Framework

--- application/views/admin/admin_footer.php
+++
@@ -1,7 +0,0 @@
-
-    <script src="<?php echo base_url();?>assets/js/jquery.js"></script>
-    <script src="<?php echo base_url();?>dist/js/bootstrap.min.js"></script>
-    <script src="<?php echo base_url();?>assets/js/holder.js"></script>
-
-</body>
-</html>
