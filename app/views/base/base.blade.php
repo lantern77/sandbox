35d32f0f7376e87340340316d137e2d790785406
Mathew Artemenko <mathew.artemenko@gmail.com>
2013-10-29 10:55:55 -0400

    Fixed the hieght of 3 wells on main page

--- app/views/base/base.blade.php
+++ app/views/base/base.blade.php
@@ -12,13 +12,13 @@
 {{-- Random things --}}
 <div class="row col-wrap">
     <div class="col-sm-4 col">
-        <div class="panel text-center">
+        <div class="well text-center">
             <h2>Open Source</h2>
             <img src="http://opensource.org/files/osi_standard_logo.png" width="175" height="200" alt="open source">
         </div>
     </div>
     <div class="col-sm-4 col">
-        <div class="panel">
+        <div class="well">
             <h4>Recent News</h4>
             <h1>News 1</h1>
             <p> Events are great</p> 
@@ -27,12 +27,19 @@
         </div>
     </div>
     <div class="col-sm-4 col">
-        <div class="panel">
+        <div class="well">
             <h4>Up Coming Tutorials</h4>
             Laravel Web Development
         </div>
     </div>
 </div>
+
+<div class="row base col-wrap">
+<!-- add a second row of wells or rounded corner divs immeadiately underneath-->
+<div class="col-sm-4 col-base"><div class="well"></div></div>
+<div class="col-sm-4 col-base"><div class="well"></div></div>
+<div class="col-sm-4 col-base"><div class="well"></div></div>
+</div><!-- close row -->
 
 @stop
 
