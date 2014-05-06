5d245d57be7954a9697b9cea6a9170cfa6114e3e
lantern77 <hoangp2@mcmaster.ca>
2014-05-06 17:49:20 -0400

    about page additions

--- app/views/about/executives.blade.php
+++ app/views/about/executives.blade.php
@@ -21,7 +21,7 @@
 	</script>
 </head>
 <div class="jumbotron">
-<h1><a href="javascipt:;" style="color:#000000" onclick=show('expand2014')>Current Executives 2014-2015</a></h1><BR>
+<h1><a href="javascipt:;" style="color:#000000" onclick=show('expand2014')><b>Current Executives 2014-2015</b></a></h1><BR>
 	<div id="expand2014">
 		<div class="row bottom-space">
 			<div class="col-md-4"><img style="/* -webkit-user-select: none */" src="/img/saif.jpg" width = 250 height = 250></div>
@@ -70,7 +70,7 @@
 		<div><a href="javascript:;" onclick=show('expand2014')>Close Executives Page<a></div>
 	</div>
 <!---Exec team of 2013-->
-<h1><a href="javascipt:;" style="color:#000000" onclick=show('expand2013')>Past Executives 2013-2014</a></h1><BR>
+<h1><a href="javascipt:;" style="color:#000000" onclick=show('expand2013')><b>Past Executives 2013-2014</b></a></h1><BR>
 	<div id="expand2013"> <!--Past section need to bee hidden-->
 		<div class="row bottom-space">
 			<div class="col-md-4"><img style="/* -webkit-user-select: none */" src="/img/mike.jpg" width = 250 height = 250></div>
